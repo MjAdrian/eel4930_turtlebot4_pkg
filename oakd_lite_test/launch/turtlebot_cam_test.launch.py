171f7f548802ad5416d35cb0aa3bf744332a5013
Adrian Fernandez <adrian23fernandez@gmail.com>
2023-04-25 12:05:00 -0400

    Updated Camera Test Launch Nodes

--- oakd_lite_test/launch/turtlebot_cam_test.launch.py
+++ oakd_lite_test/launch/turtlebot_cam_test.launch.py
@@ -5,8 +5,14 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='oakd_lite_test',
-            executable='oakd_sub_test',
-            name='oakd_sub_test',
+            executable='oakd_pub_test',
+            name='oakd_pub_test',
             output='screen'
         ),
+        # Node(
+        #     package='oakd_lite_test',
+        #     executable='oakd_sub_test',
+        #     name='oakd_sub_test',
+        #     output='screen'
+        # ),
     ])
