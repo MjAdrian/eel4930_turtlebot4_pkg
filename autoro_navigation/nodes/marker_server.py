e6812e7bb4370e9f87dfa688accc1a7071496497
Cameron Brown <me@cbrxyz.com>
2023-05-01 19:04:22 -0400

    First trial of marker server and customized Rviz for marker server

--- autoro_navigation/nodes/marker_server.py
+++ autoro_navigation/nodes/marker_server.py
@@ -13,11 +13,13 @@
 from __future__ import annotations
 
 import sys
+import rich
 
 import rclpy
+import random
 from rclpy.node import Node
 from interactive_markers import InteractiveMarkerServer
-from visualization_msgs.msg import InteractiveMarker
+from visualization_msgs.msg import InteractiveMarker, Marker, InteractiveMarkerControl
 
 
 class MarkerServer(Node):
@@ -27,28 +29,57 @@ class MarkerServer(Node):
     def __init__(self):
         super().__init__("marker_server")
         self.server = None
-        self.timer = self.create_timer(1, self.create_sphere)
+        self.timer = self.create_timer(30, self.create_sphere)
 
     def set_server(self, server: InteractiveMarkerServer):
         self.server = server
+        self.create_sphere()
+
+    def process_feedback(self, feedback):
+        print(f"received {type(feedback)}")
 
     def create_sphere(self):
         marker = InteractiveMarker()
-        marker.header.frame_id = "base_link"
-        marker.scale = 0.2
-
+        marker.header.frame_id = "map"
         marker.pose.position.x = 0.0
         marker.pose.position.y = 0.0
-        marker.pose.position.z = 0.0
-
+        marker.pose.position.z = 1.0
         marker.name = "test name"
+
+        viz_marker = Marker()
+        viz_marker.type = Marker.SPHERE
+        viz_marker.scale.x = 0.3
+        viz_marker.scale.y = 0.3
+        viz_marker.scale.z = 0.3
+        viz_marker.color.r = random.random()
+        viz_marker.color.g = random.random()
+        viz_marker.color.b = random.random()
+        viz_marker.color.a = 1.0
+
+        control_marker = InteractiveMarkerControl()
+        control_marker.always_visible = True
+        control_marker.markers.append(viz_marker)
+        marker.controls.append(control_marker)
+
+        movement_marker = InteractiveMarkerControl()
+        movement_marker.name = "move_xy"
+        movement_marker.orientation.w = 1.0
+        movement_marker.orientation.x = 0.0
+        movement_marker.orientation.y = 1.0
+        movement_marker.orientation.z = 0.0
+        movement_marker.interaction_mode = InteractiveMarkerControl.MOVE_PLANE
+        movement_marker.always_visible = True
+        marker.controls.append(movement_marker)
+
         if server is None:
             raise ValueError(
                 "The interactive marker server has not yet been initialized."
             )
 
-        server.insert(marker)
+        server.insert(marker, feedback_callback = self.process_feedback)
         server.applyChanges()
+        rich.print(f"Just inserted marker with {len(marker.controls)} controls...")
+        rich.print(marker.controls)
 
 
 if __name__ == "__main__":
