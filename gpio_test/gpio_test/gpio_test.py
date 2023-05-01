37a5434d176f6b085eb1ca7d1260a5241581ad54
Adrian Fernandez <adrian23fernandez@gmail.com>
2023-05-01 17:33:56 -0400

    Bug FIxes with WIndows Git

--- gpio_test/gpio_test/gpio_test.py
+++ gpio_test/gpio_test/gpio_test.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+# set pin mode to GPIO
 GPIO.setmode(GPIO.BCM)
 
 # Define the GPIO pins for the servos
@@ -20,19 +21,25 @@ servo2 = GPIO.PWM(SERVO2_PIN, pwm_frequency)
 
 # Start the PWM for each servo with a 0% duty cycle
 #servo1.start(0)
-servo2.start(0)
+servo2.start(5)
 
 try:
     while True:
+        #print('0')
+
         # Move the servos to different angles (0 to 180 degrees)
-        for angle in range(0, 271, 10):
-            duty_cycle = (angle / 180.0) * 10 + 2
+        for angle in range(90, 60, -1):
+            servo2.ChangeDutyCycle((90/180)*10)
+            time.sleep(0.5)
+
+            duty_cycle = (angle / 180.0) * 10 #+ 2
+            print(f"duty: {duty_cycle}")
             #servo1.ChangeDutyCycle(duty_cycle)
             servo2.ChangeDutyCycle(duty_cycle)
             print(angle)
             time.sleep(0.5)
 
-        break
+        continue
 
         # Move the servos back to the starting position (180 to 0 degrees)
         for angle in range(360, -1, -10):
@@ -42,12 +49,13 @@ try:
             time.sleep(0.5)
 
     while True:
-        duty_cycle = 2
+        duty_cycle = 50
         servo2.ChangeDutyCycle(duty_cycle)
         pass
 
 except KeyboardInterrupt:
     # Stop the program when Ctrl+C is pressed
+    servo2.ChangeDutyCycle((90/180)*10)
     print("Exiting...")
 
 finally:
@@ -55,4 +63,3 @@ finally:
     #servo1.stop()
     servo2.stop()
     GPIO.cleanup()
-
