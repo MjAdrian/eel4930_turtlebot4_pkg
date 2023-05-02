6cd16e684a1f5f8cba07c90a8a0b2b322d9728bd
Cameron Brown <me@cbrxyz.com>
2023-05-02 12:04:25 -0400

    Markers are able to spawn from config file

--- autoro_navigation/nodes/marker_server.py
+++ autoro_navigation/nodes/marker_server.py
@@ -12,14 +12,16 @@
 
 from __future__ import annotations
 
+import random
 import sys
-import rich
 
 import rclpy
-import random
-from rclpy.node import Node
+import rich
+import yaml
 from interactive_markers import InteractiveMarkerServer
-from visualization_msgs.msg import InteractiveMarker, Marker, InteractiveMarkerControl
+from rclpy.node import Node
+from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, Marker, InteractiveMarkerFeedback
+from ament_index_python.packages import get_package_share_directory
 
 
 class MarkerServer(Node):
@@ -29,22 +31,33 @@ class MarkerServer(Node):
     def __init__(self):
         super().__init__("marker_server")
         self.server = None
-        self.timer = self.create_timer(30, self.create_sphere)
 
     def set_server(self, server: InteractiveMarkerServer):
         self.server = server
-        self.create_sphere()
+        self.read_points()
 
-    def process_feedback(self, feedback):
-        print(f"received {type(feedback)}")
+    def read_points(self) -> None:
+        autoro_share = get_package_share_directory("autoro_navigation")
+        with open(f"{autoro_share}/config/patrol_path.yaml", "r") as f:
+            file_contents = yaml.safe_load(f)
+            if "points" not in file_contents:
+                raise ValueError(
+                    "Patrol path configuration file does not contain list of path points to make a path."
+                )
+            points = file_contents["points"]
+            for point in points:
+                self.create_sphere(point[0], point[1])
 
-    def create_sphere(self):
+    def process_feedback(self, feedback: InteractiveMarkerFeedback):
+        print(f"received {feedback.marker_name}, {feedback.control_name}, {feedback.pose.position.x}, {feedback.pose.position.y}")
+
+    def create_sphere(self, x: float, y: float):
         marker = InteractiveMarker()
         marker.header.frame_id = "map"
-        marker.pose.position.x = 0.0
-        marker.pose.position.y = 0.0
-        marker.pose.position.z = 1.0
-        marker.name = "test name"
+        marker.pose.position.x = float(x)
+        marker.pose.position.y = float(y)
+        marker.pose.position.z = 0.0
+        marker.description = f"{x}, {y}"
 
         viz_marker = Marker()
         viz_marker.type = Marker.SPHERE
@@ -54,6 +67,9 @@ class MarkerServer(Node):
         viz_marker.color.r = random.random()
         viz_marker.color.g = random.random()
         viz_marker.color.b = random.random()
+        viz_marker.pose.position.x = float(x)
+        viz_marker.pose.position.y = float(y)
+        viz_marker.pose.position.z = 0.0
         viz_marker.color.a = 1.0
 
         control_marker = InteractiveMarkerControl()
@@ -62,13 +78,14 @@ class MarkerServer(Node):
         marker.controls.append(control_marker)
 
         movement_marker = InteractiveMarkerControl()
-        movement_marker.name = "move_xy"
+        movement_marker.name = f"move_xy_{x}{y}"
         movement_marker.orientation.w = 1.0
         movement_marker.orientation.x = 0.0
         movement_marker.orientation.y = 1.0
         movement_marker.orientation.z = 0.0
         movement_marker.interaction_mode = InteractiveMarkerControl.MOVE_PLANE
         movement_marker.always_visible = True
+        movement_marker.markers.append(viz_marker)
         marker.controls.append(movement_marker)
 
         if server is None:
@@ -76,7 +93,7 @@ class MarkerServer(Node):
                 "The interactive marker server has not yet been initialized."
             )
 
-        server.insert(marker, feedback_callback = self.process_feedback)
+        server.insert(marker, feedback_callback=self.process_feedback)
         server.applyChanges()
         rich.print(f"Just inserted marker with {len(marker.controls)} controls...")
         rich.print(marker.controls)
