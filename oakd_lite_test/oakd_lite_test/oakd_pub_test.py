fdb041c1a96139a2feaed95d1a787884ed7ceb68
Adrian Fernandez <adrian23fernandez@gmail.com>
2023-05-02 23:38:49 -0400

    going back

--- oakd_lite_test/oakd_lite_test/oakd_pub_test.py
+++ oakd_lite_test/oakd_lite_test/oakd_pub_test.py
@@ -9,10 +9,9 @@ class OakDLitePublisher(Node):
     def __init__(self):
         super().__init__('oakd_lite_publisher')
 
-        # create publishers
-        self.rgb_publisher_ = self.create_publisher(Image, '/oakd_lite_camera/rgb_image', 10)
-        self.depth_publisher_ = self.create_publisher(Image, '/oakd_lite_camera/depth_image', 10)
-
+        # create publisher
+        self.publisher_ = self.create_publisher(Image, '/oakd_lite_camera/rgb_image', 10)
+        
         # set up CV Bridge
         self.bridge = CvBridge()
 
@@ -24,46 +23,23 @@ class OakDLitePublisher(Node):
         xout_rgb = self.pipeline.create(dai.node.XLinkOut)
         xout_rgb.setStreamName("rgb")
 
-        # Set up depth images
-        cam_mono = self.pipeline.create(dai.node.MonoCamera)
-        cam_depth = self.pipeline.create(dai.node.StereoDepth)
-        xout_depth = self.pipeline.create(dai.node.XLinkOut)
-        xout_depth.setStreamName("depth")
-
-        cam_depth.setConfidenceThreshold(200)
-        cam_depth.setMedianFilter(dai.StereoDepthProperties.MedianFilter.KERNEL_7x7)
-
-        # set up mono camera
-        cam_mono.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
-
         # image properties
         cam_rgb.setPreviewSize(300, 300)
         cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
         cam_rgb.setInterleaved(False)
 
-        # set up output link for RGB images
+        # set up output link
         cam_rgb.preview.link(xout_rgb.input)
-
-        # set up output link for depth images
-        cam_rgb.preview.link(cam_depth.left)
-        cam_mono.out.link(cam_depth.right)
-        cam_depth.depth.link(xout_depth.input)
 
         # set up device instance
         self.device = dai.Device(self.pipeline)
         self.q_rgb = self.device.getOutputQueue("rgb", 4, blocking=False)
-        self.q_depth = self.device.getOutputQueue("depth", 4, blocking=False)
 
     def publish_camera_data(self):
-        # get msg, convert to CV img msg then publish RGB images
+        # get msg, convert to CV img msg then publish
         frame_rgb = self.q_rgb.get().getCvFrame()
-        image_message_rgb = self.bridge.cv2_to_imgmsg(frame_rgb, encoding="bgr8")
-        self.rgb_publisher_.publish(image_message_rgb)
-
-        # get msg, convert to CV img msg then publish depth images
-        frame_depth = self.q_depth.get().getCvFrame()
-        image_message_depth = self.bridge.cv2_to_imgmsg(frame_depth, encoding="mono16")
-        self.depth_publisher_.publish(image_message_depth)
+        image_message = self.bridge.cv2_to_imgmsg(frame_rgb, encoding="bgr8")
+        self.publisher_.publish(image_message)
 
 def main(args=None):
     rclpy.init(args=args)
